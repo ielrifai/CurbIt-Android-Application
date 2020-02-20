8bcf67d47e70292d59f2f3b862734ce532d81860
cbauce <cbauce@uwo.ca>
2020-02-20 17:58:54 -0500

    change navigation menu

--- app/src/main/java/se3350/habittracker/ui/information/InformationFragment.java
+++ app/src/main/java/se3350/habittracker/ui/information/InformationFragment.java
@@ -1,4 +1,4 @@
-package se3350.habittracker.ui.notifications;
+package se3350.habittracker.ui.information;
 
 import android.os.Bundle;
 import android.view.LayoutInflater;
@@ -6,25 +6,25 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
-import androidx.annotation.Nullable;
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
 
 import se3350.habittracker.R;
 
-public class NotificationsFragment extends Fragment {
+public class InformationFragment extends Fragment {
 
-    private NotificationsViewModel notificationsViewModel;
+    private InformationViewModel informationViewModel;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-        notificationsViewModel =
-                ViewModelProviders.of(this).get(NotificationsViewModel.class);
-        View root = inflater.inflate(R.layout.fragment_notifications, container, false);
-        final TextView textView = root.findViewById(R.id.text_notifications);
-        notificationsViewModel.getText().observe(this, new Observer<String>() {
+        informationViewModel =
+                ViewModelProviders.of(this).get(InformationViewModel.class);
+        View root = inflater.inflate(R.layout.fragment_information, container, false);
+        final TextView textView = root.findViewById(R.id.text_information);
+        informationViewModel.getText().observe(this, new Observer<String>() {
             @Override
             public void onChanged(@Nullable String s) {
                 textView.setText(s);
