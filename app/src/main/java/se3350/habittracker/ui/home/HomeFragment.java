cc51bdaac781f6dde1b5d22d72db6b6ab78a392e
cbauce <cbauce@uwo.ca>
2020-02-24 15:25:53 -0500

    added add button to home. change title of habit list to Habit List Fixed the activity main layout fragment bottom

--- app/src/main/java/se3350/habittracker/ui/home/HomeFragment.java
+++ app/src/main/java/se3350/habittracker/ui/home/HomeFragment.java
@@ -4,7 +4,9 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
@@ -28,9 +30,11 @@ public class HomeFragment extends Fragment {
 
         View root = inflater.inflate(R.layout.fragment_home, container, false);
         ListView habitListView = root.findViewById(R.id.list_habit);
+        Button addButton = root.findViewById(R.id.btn_add);
 
         List<Habit> habits = new ArrayList<>();
 
+        // Set the habit list adapter
         HabitListAdapter adapter = new HabitListAdapter(getContext(), habits);
         habitListView.setAdapter(adapter);
 
@@ -39,14 +43,6 @@ public class HomeFragment extends Fragment {
         Habit newHabit = new Habit("habit", "hello");
         AppDatabase db = AppDatabase.getInstance(getContext());
         HabitDao habitDao = db.habitDao();
-
-        // TODO: Remove once add habit is done
-        // asynchronous insert using an executor
-        Executor myExecutor = Executors.newSingleThreadExecutor();
-        myExecutor.execute(() -> {
-            habitDao.insertAll(newHabit);
-        });
-
 
         // get habits from database
         LiveData<Habit[]> habitList = habitDao.getAll();
@@ -57,6 +53,21 @@ public class HomeFragment extends Fragment {
             habits.addAll(Arrays.asList(newHabits));
             adapter.notifyDataSetChanged();
         });
+
+
+        // Set add button to open the add habit form
+        addButton.setOnClickListener(event -> {
+            // TODO: Open the add habit form activity
+            Toast.makeText(getContext(),"Add a habit!", Toast.LENGTH_SHORT).show();
+        });
+
+        // TODO: Remove once add habit is done
+        // asynchronous insert using an executor
+        Executor myExecutor = Executors.newSingleThreadExecutor();
+        myExecutor.execute(() -> {
+            habitDao.insertAll(newHabit);
+        });
+
 
         return root;
     }
