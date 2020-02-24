c704db4497ef8fba050410d757a718bee79a1b61
cbauce <cbauce@uwo.ca>
2020-02-24 05:00:18 -0500

    add habit list on Home and its adapter
    - habits are gotten from the database
    - list is updated when data is changed

--- app/src/main/java/se3350/habittracker/ui/home/HomeFragment.java
+++ app/src/main/java/se3350/habittracker/ui/home/HomeFragment.java
@@ -1,18 +1,16 @@
 package se3350.habittracker.ui.home;
 
-import android.os.Build;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
+import android.widget.ListView;
 
 import androidx.annotation.NonNull;
-import androidx.annotation.RequiresApi;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.LiveData;
-import androidx.lifecycle.Observer;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.concurrent.Executor;
@@ -22,39 +20,42 @@ import se3350.habittracker.AppDatabase;
 import se3350.habittracker.Habit;
 import se3350.habittracker.HabitDao;
 import se3350.habittracker.R;
+import se3350.habittracker.adapters.HabitListAdapter;
 
 public class HomeFragment extends Fragment {
 
     public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 
         View root = inflater.inflate(R.layout.fragment_home, container, false);
+        ListView habitListView = root.findViewById(R.id.list_habit);
 
-        final TextView title = root.findViewById(R.id.title_habit_list);
+        List<Habit> habits = new ArrayList<>();
 
+        HabitListAdapter adapter = new HabitListAdapter(getContext(), habits);
+        habitListView.setAdapter(adapter);
+
+
+        // Get the database and user dao
         Habit newHabit = new Habit("habit", "hello");
-
         AppDatabase db = AppDatabase.getInstance(getContext());
-
         HabitDao habitDao = db.habitDao();
 
+        // TODO: Remove once add habit is done
         // asynchronous insert using an executor
         Executor myExecutor = Executors.newSingleThreadExecutor();
         myExecutor.execute(() -> {
             habitDao.insertAll(newHabit);
         });
 
-        // asynchronous get using live data
+
+        // get habits from database
         LiveData<Habit[]> habitList = habitDao.getAll();
 
-        // observe the data, set title each time it's updated
-        habitList.observe(getViewLifecycleOwner(), habits -> {
-            if(habits.length > 0){
-                String str = "";
-                for (Habit habit : habits){
-                    str += habit.name + " " + habit.description + "\n";
-                }
-                title.setText(str);
-            }
+        // observe the data, refresh habit list view each time it's updated
+        habitList.observe(getViewLifecycleOwner(), newHabits -> {
+            habits.clear();
+            habits.addAll(Arrays.asList(newHabits));
+            adapter.notifyDataSetChanged();
         });
 
         return root;
