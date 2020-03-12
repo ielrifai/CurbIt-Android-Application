1ae9feaea447f8a0e85b03e37afca81d01bf76d8
Jamie Goff <JamieGoff@dyn172-30-199-60.reznet.wireless.uwo.pri>
2020-03-11 20:52:52 -0400

    Added New Activities for Remainder of Tutorial

--- app/src/main/java/se3350/habittracker/activities/Step1TutorialActivity.java
+++ app/src/main/java/se3350/habittracker/activities/Step1TutorialActivity.java
@@ -2,13 +2,19 @@ package se3350.habittracker.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.method.ScrollingMovementMethod;
+import android.widget.TextView;
+
 import se3350.habittracker.R;
 
 public class Step1TutorialActivity extends AppCompatActivity {
 
+    private TextView text;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_step1_tutorial);
+        text = findViewById(R.id.tut_step1_steve_says);
+        text.setMovementMethod(new ScrollingMovementMethod());
     }
 }
---
+++ app/src/main/java/se3350/habittracker/activities/Step4TutorialActivity.java
@@ -0,0 +1,16 @@
+package se3350.habittracker.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import se3350.habittracker.R;
+
+public class Step4TutorialActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_step4_tutorial);
+    }
+}
