f01506446fac1dc568ab90057e6ae40b9dad37a2
Jamie Goff <JamieGoff@dyn172-30-199-60.reznet.wireless.uwo.pri>
2020-03-11 21:05:01 -0400

    Formatted Step 2 of Tutorial

--- app/src/main/java/se3350/habittracker/activities/Step2TutorialActivity.java
+++ app/src/main/java/se3350/habittracker/activities/Step2TutorialActivity.java
@@ -3,14 +3,19 @@ package se3350.habittracker.activities;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.text.method.ScrollingMovementMethod;
+import android.widget.TextView;
 
 import se3350.habittracker.R;
 
 public class Step2TutorialActivity extends AppCompatActivity {
+    private TextView text;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_step2_tutorial);
+        text = findViewById(R.id.tut_step2_steve_says);
+        text.setMovementMethod(new ScrollingMovementMethod());
     }
 }
