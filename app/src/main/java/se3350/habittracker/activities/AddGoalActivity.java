5215ed3a6fff06b798090537f46f0f692c1476c8
ielrifai <46636203+ielrifai@users.noreply.github.com>
2020-03-15 15:59:15 -0400

    Added AddGoalActivity and xml

---
+++ app/src/main/java/se3350/habittracker/activities/AddGoalActivity.java
@@ -0,0 +1,69 @@
+package se3350.habittracker.activities;
+
+
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import java.util.concurrent.Executor;
+import java.util.concurrent.Executors;
+
+import se3350.habittracker.AppDatabase;
+import se3350.habittracker.R;
+import se3350.habittracker.daos.GoalDao;
+import se3350.habittracker.models.Goal;
+
+public class AddGoalActivity extends ActionBarActivity {
+
+    //vars to hold input data
+    String goalName, goalDescription;
+
+    EditText goalNameInput, goalDescriptionInput;
+
+    Button submitButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_goal);
+        // getSupportActionBar().setDisplayShowHomeEnabled(true);
+
+        goalNameInput = (EditText) findViewById(R.id.goal_name);
+        goalDescriptionInput = (EditText) findViewById(R.id.goal_description);
+
+        submitButton = (Button) findViewById(R.id.submit_btn);
+
+        submitButton.setOnClickListener(v -> {
+            //save habit info
+            goalName = goalNameInput.getText().toString();
+            goalDescription = goalDescriptionInput.getText().toString();
+
+            if(goalName.length() == 0 || goalDescription.length() == 0){
+                Toast.makeText(getApplicationContext(), R.string.error_add_goal,Toast.LENGTH_SHORT).show();
+                return;
+            }
+
+            //add goal to db
+            Goal newGoal = new Goal(goalName, goalDescription);
+
+            AppDatabase db = AppDatabase.getInstance(this);
+
+            GoalDao goalDao = db.goalDao();
+
+            // asynchronous insert using an executor
+            //to use Java 8 - go to project settings and change target compatibility to 1.8
+            Executor myExecutor = Executors.newSingleThreadExecutor();
+            myExecutor.execute(() -> {
+                goalDao.insertAll(newGoal);
+            });
+
+            onBackPressed();
+        });
+
+    }
+
+
+
+}
