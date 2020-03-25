07caf17f43133e64fa41083c217e412f7f47054c
ahameed1 <47093739+ahameed1@users.noreply.github.com>
2020-03-25 04:09:38 -0400

    notification setup

--- app/src/main/java/se3350/habittracker/activities/JournalNotificationActivity.java
+++ app/src/main/java/se3350/habittracker/activities/JournalNotificationActivity.java
@@ -1,16 +1,68 @@
 package se3350.habittracker.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.NotificationCompat;
+import androidx.core.app.NotificationManagerCompat;
 
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.app.TaskStackBuilder;
+import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
+import android.content.BroadcastReceiver;
 
 import se3350.habittracker.R;
 
+import static android.app.Notification.EXTRA_NOTIFICATION_ID;
+
 public class JournalNotificationActivity extends AppCompatActivity {
+    private int notificationId, savedNotificationId;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_journal_notification);
+        private void createNotificationChannel() {
+            // Create the NotificationChannel, but only on API 26+ because
+            // the NotificationChannel class is new and not in the support library
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                CharSequence name = getString(R.string.channel_name);
+                String description = getString(R.string.channel_description);
+                int importance = NotificationManager.IMPORTANCE_DEFAULT;
+                NotificationChannel channel = new NotificationChannel(CHANNEL_ID, name, importance);
+                channel.setDescription(description);
+                // Register the channel with the system; you can't change the importance
+                // or other notification behaviors after this
+                NotificationManager notificationManager = getSystemService(NotificationManager.class);
+                notificationManager.createNotificationChannel(channel);
+            }
+        }
+
+        // Create an Intent for the activity you want to start
+        Intent resultIntent = new Intent(this, JournalNotificationActivity.class);
+// Create the TaskStackBuilder and add the intent, which inflates the back stack
+        TaskStackBuilder stackBuilder = TaskStackBuilder.create(this);
+        stackBuilder.addNextIntentWithParentStack(resultIntent);
+// Get the PendingIntent containing the entire back stack
+        PendingIntent resultPendingIntent =
+                stackBuilder.getPendingIntent(0, PendingIntent.FLAG_UPDATE_CURRENT);
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(this, CHANNEL_ID)
+                .setSmallIcon(R.drawable.ic_notifications_black_24dp)
+                .setContentTitle("Journal Entry")
+                .setContentText("Remember to fill out your daily journal!")
+                .setStyle(new NotificationCompat.BigTextStyle()
+                        .bigText("REMINDER"))
+                .setPriority(NotificationCompat.PRIORITY_DEFAULT);
+        builder.setContentIntent(resultPendingIntent);
+        builder.setAutoCancel(true);
+
+        NotificationManagerCompat notificationManager = NotificationManagerCompat.from(this);
+        notificationManager.notify(notificationId, builder.build());
+        savedNotificationId = notificationId;
+
+
     }
 }
