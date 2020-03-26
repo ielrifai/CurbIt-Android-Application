d39542e080b921d9119238c39ee2f8c03723a730
ielrifai <46636203+ielrifai@users.noreply.github.com>
2020-03-26 19:37:16 -0400

    Changed search functionality test directory and edited test functions

--- app/src/test/java/se3350/habittracker/JournalListActivityTest.java
+++ app/src/test/java/se3350/habittracker/JournalListActivityTest.java
@@ -1,61 +1,56 @@
 package se3350.habittracker;
 
-import androidx.test.ext.junit.runners.AndroidJUnit4;
-
 import org.junit.Test;
-import org.junit.runner.RunWith;
 
 import se3350.habittracker.activities.JournalListActivity;
 
-import static org.hamcrest.Matchers.is;
-import static org.junit.Assert.assertThat;
+import static org.junit.Assert.assertEquals;
 
-/**
- * Instrumented test, which will execute on an Android device.
- *
- * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
- */
-@RunWith(AndroidJUnit4.class)
 public class JournalListActivityTest {
 
     @Test
-    public void testSearchValidator() {
+    public void testSearchValidator_CorrectFormat() {
         System.out.println("search validator for correct format");
-        String newText = "03/16/20";
+        String newText = "3/16/20";
+        boolean expectedResult = true;
         boolean result = JournalListActivity.searchValidator(newText);
-        assertThat(result, is(true));
+        assertEquals(expectedResult, result);
     }
 
     @Test
-    public void testSearchValidator1() {
+    public void testSearchValidator_EmptySearch() {
         System.out.println("search validator for empty search");
         String newText = "";
+        boolean expectedResult = false;
         boolean result = JournalListActivity.searchValidator(newText);
-        assertThat(result, is(false));
+        assertEquals(expectedResult, result);
     }
 
     @Test
-    public void testSearchValidator2() {
+    public void testSearchValidator_WrongFormat1() {
         System.out.println("search validator for wrong format");
         String newText = "3-16-20";
+        boolean expectedResult = false;
         boolean result = JournalListActivity.searchValidator(newText);
-        assertThat(result, is(false));
+        assertEquals(expectedResult, result);
     }
 
     @Test
-    public void testSearchValidator3() {
+    public void testSearchValidator_WrongFormat2() {
         System.out.println("search validator for wrong format");
         String newText = "3/16/2020";
+        boolean expectedResult = false;
         boolean result = JournalListActivity.searchValidator(newText);
-        assertThat(result, is(false));
+        assertEquals(expectedResult, result);
     }
 
     @Test
-    public void testSearchValidator4() {
+    public void testSearchValidator_WrongFormat3() {
         System.out.println("search validator for wrong format");
         String newText = "03/16/20";
+        boolean expectedResult = false;
         boolean result = JournalListActivity.searchValidator(newText);
-        assertThat(result, is(false));
+        assertEquals(expectedResult, result);
     }
 
 
