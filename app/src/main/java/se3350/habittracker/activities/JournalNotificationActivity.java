bd5a3c2441a616ac4bf5246945dcd5c9560ddb5e
Jamie Goff <JamieGoff@B92.local>
2020-03-26 12:11:40 -0400

    Got Notifications Working and Added Outline of Code for Switch

--- app/src/main/java/se3350/habittracker/activities/JournalNotificationActivity.java
+++ app/src/main/java/se3350/habittracker/activities/JournalNotificationActivity.java
@@ -12,6 +12,7 @@ import android.content.Intent;
 import android.os.Build;
 import android.os.Bundle;
 import android.widget.Button;
+import android.widget.CompoundButton;
 import android.widget.Switch;
 import android.widget.TextView;
 import android.widget.TimePicker;
@@ -27,7 +28,7 @@ import static android.app.NotificationChannel.DEFAULT_CHANNEL_ID;
 public class JournalNotificationActivity extends AppCompatActivity {
     private int notificationId, savedNotificationId;
     TimePicker setTime;
-    Switch sendNotification;
+    Switch notificationSwitch;
     JournalEntryDao journalEntryDao;
     TextView habitNotificationSettings;
     TextView habitNotificationDescription;
@@ -44,47 +45,79 @@ public class JournalNotificationActivity extends AppCompatActivity {
         setContentView(R.layout.activity_journal_notification);
         saveNotificationSettingsBtn = findViewById(R.id.save_notification_settings_button);
         setTime = findViewById(R.id.notification_time_picker);
-        sendNotification = findViewById(R.id.habit_notification_switch);
+        notificationSwitch = findViewById(R.id.habit_notification_switch);
         habitNotficationTitle = findViewById(R.id.habit_notification_title);
         habitNotificationDescription = findViewById(R.id.habit_notification_description);
         habitNotificationSettings = findViewById(R.id.habit_notification_title);
+        createNotificationChannel();
 
         //get daos
         AppDatabase db = AppDatabase.getInstance(getBaseContext());
         journalEntryDao = db.journalEntryDao();
         habitDao = db.habitDao();
-        Intent resultIntent = new Intent(this, JournalNotificationActivity.class);
+
+
+        Intent resultIntent = new Intent(this, JournalNotificationActivity.class);  //this currently bypasses the lock screen of the app - consider revising
         // Create the TaskStackBuilder and add the intent, which inflates the back stack
+
+        resultIntent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+        PendingIntent pendingIntent = PendingIntent.getActivity(this, 0, resultIntent, 0);
+
+
+        /*
         TaskStackBuilder stackBuilder = TaskStackBuilder.create(this);
         stackBuilder.addNextIntentWithParentStack(resultIntent);
         // Get the PendingIntent containing the entire back stack
         PendingIntent resultPendingIntent =
                 stackBuilder.getPendingIntent(0, PendingIntent.FLAG_UPDATE_CURRENT);
-        NotificationCompat.Builder builder = new NotificationCompat.Builder(this, DEFAULT_CHANNEL_ID)
+
+         */
+
+
+
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(this, "main_notifications")  //check channel ID
                 .setSmallIcon(R.drawable.ic_notifications_black_24dp)
                 .setContentTitle("Journal Entry")
                 .setContentText("Remember to fill out your daily journal!")
                 .setStyle(new NotificationCompat.BigTextStyle()
                         .bigText("REMINDER"))
                 .setPriority(NotificationCompat.PRIORITY_DEFAULT);
-        builder.setContentIntent(resultPendingIntent);
+        builder.setContentIntent(pendingIntent);
         builder.setAutoCancel(true);
 
         NotificationManagerCompat notificationManager = NotificationManagerCompat.from(this);
         notificationManager.notify(notificationId, builder.build());
-        savedNotificationId = notificationId;
+        //savedNotificationId = notificationId;
+
+
+
+        notificationSwitch.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton compoundButton, boolean isChecked) {
+                if(isChecked) {
+                    //turn on notifications for the habit
+                    System.out.println("Notifications turned on");  //remove this after we get it working
+                }
+
+                else {
+                    //turn off notifications for the habit
+                    System.out.println("Notifications turned off");  //remove this after we get it working
+                }
+
+            }
+        });
 
     }
 
 
-    private void createNotificationChannel() {
+    private void createNotificationChannel() {  //this should probably be called on opening app
             // Create the NotificationChannel, but only on API 26+ because
             // the NotificationChannel class is new and not in the support library
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
             CharSequence name = getString(R.string.channel_name);
             String description = getString(R.string.channel_description);
             int importance = NotificationManager.IMPORTANCE_DEFAULT;
-            NotificationChannel channel = new NotificationChannel(DEFAULT_CHANNEL_ID, name, importance);
+            NotificationChannel channel = new NotificationChannel("main_notifications", name, importance);
             channel.setDescription(description);
             // Register the channel with the system; you can't change the importance
             // or other notification behaviors after this
@@ -92,6 +125,5 @@ public class JournalNotificationActivity extends AppCompatActivity {
             notificationManager.createNotificationChannel(channel);
             }
         }
-
     }
 
